eea5d4380156fa56c0d587981dcd9b6711cd2067
MikroMike <dev@mikromike.email>
2022-01-25 14:22:33 +0200

    Prospect index updated and @if loop fixed.

--- resources/views/prospect/index.blade.php
+++ resources/views/prospect/index.blade.php
@@ -1,16 +1,18 @@
 @extends('parts.app')
 @section('body')
-
 <select class="mt-2 mb-4" name="codelist">
-
   @foreach ($bsscodes as $code)
-    @foreach ($totalcount as $count)
-    <option value="{{ $code['nameFI'] }} ">
-      {{ $code['code'] }} ..... {{ $code['nameFI'] }} ... Total 
-      {{ $count['code'] === $code['code'] ?  $count['total']  : @continue  }}
+      @foreach ($totalcount as $count)
+          @if ($count['code'] === $code['code'])
+            <option value="{{ $code['nameFI'] }} ">
+            {{ $code['code'] }} ..... {{ $code['nameFI'] }} ... {{__('Kokonaismäärä')}}
+            {{ $count['total'] }}
+      @else
+          @continue
+      @endif
     </option>
     @endforeach
-  @endforeach
+ @endforeach
 </select>
 @include('prospect.table')
 @endsection
