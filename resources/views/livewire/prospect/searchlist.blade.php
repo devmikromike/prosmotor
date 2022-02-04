d01f3595181846bfa37195c3bc8241d0af2709f4
MikroMike <dev@mikromike.email>
2022-02-04 12:21:55 +0200

    main new welcome layout.

--- resources/views/livewire/prospect/searchlist.blade.php
+++ resources/views/livewire/prospect/searchlist.blade.php
@@ -8,8 +8,15 @@
     </div>
     @endif
     @if(!empty($newproslist))
-      <div class="mt-4 mb-4">
-        <div class="flex flex-1 ">
+  {{--    <div class="mt-4 mb-4"> --}}
+  {{--      <select class="mt-2 mb-4" name="codelist"> --}}
+  {{--          @foreach ($bsscodes as $code) --}}
+  {{--              <option class="mr-4" value="{{ $code['code'] }} "> --}}
+  {{--                  {{ $code['code']  }} ..... {{ $code['nameFI'] }} --}}
+  {{--              </option>
+  {{--         @endforeach --}}
+    {{--    </select> --}}
+        <div class="flex flex-col bg-gray-200 dark:bg-gray-900 py-4 sm:pt-0 ">
             @foreach($newproslist as $prospects)
                 @foreach($prospects as $prospect)
                   @include ('livewire.prospect.table')
