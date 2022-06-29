17e2a95e2ff90829019c67e7e22f5d7b70dfce0e
MikroMike <dev@mikromike.email>
2022-06-29 13:47:09 +0300

    postal code dropdown.

--- app/Http/Livewire/Search/City.php
+++ app/Http/Livewire/Search/City.php
@@ -5,31 +5,35 @@ namespace App\Http\Livewire\Search;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\CityList;
+use App\Models\PostCodeList;
 
 class City extends Component
 {
    use WithPagination;
    public $city;
+   private$cities;
    private $citynames;
+   public $postalCodes;
+   public $code;
+   public $selectedCity = NULL;
 
     public function mount()
     {
-        // $citynames = (new CityList())->loadCities();
+         $this->cities = CityList::orderBy('name', 'asc')->paginate(10);
+         $this->postalCodes = collect();
     }
-    public function cityDropdown()
+    public function updatedSelectedCity($city)
     {
-
+        if (!is_null($city)) {
+            $this->postalCodes = PostCodeList::where('city_lists_id', $city)
+            ->orderBy('postalCode', 'asc')
+            ->get();
+        }
     }
     public function render()
     {
-        $cities = CityList::orderBy('name', 'asc');
-        $citynames = $cities->paginate(10);
-        //   $this->citynames = $citynames->toArray();
-        $this->citynames = $citynames;
-
-
-        return view('livewire.search.city', [
-          'citynames' => $this->citynames
-        ]);
+      return view('livewire.search.city', [
+        'citynames' => $this->cities
+      ]);
     }
 }
--- resources/views/livewire/search/city.blade.php
+++ resources/views/livewire/search/city.blade.php
@@ -1,13 +1,12 @@
 
 <div class="flex-col ml-4  bg-gray-200">
-  <div class="mb-4 flex flex-3/4 ">
 
+  <div class="mb-4 flex flex-3/4 ">
     @if(!empty($citynames))
 
-       <select class="mt-2 mb-4" value =" "  >
+       <select class="mt-2 mb-4"  wire:model="selectedCity"  >
            <option class="mr-4" value="" selected>  </option>
           @foreach($citynames as $city)
-
               @if(!empty($city))
                 <option class="mr-4" value="{{ $city->id }}  ">
                   {{ $city->name }}
@@ -15,11 +14,21 @@
               @endif
           @endforeach
       </select>
-
   </div>
       <div class="  mb-4 mr-2 flex flex-2/4 ">
-          {{ $citynames->links() }}
+     {{--      {{ $citynames->links() }} --}}
       </div>
 
     @endif
+
+      @if (!is_null($selectedCity))
+ <select class="mt-2 mb-4"  >
+      <option class="mr-4" value="" selected>  </option>
+        @foreach($postalCodes as $code)
+        <option class="mr-4" value="{{ $code->id  }}  ">
+          {{ $code->postalCode }}
+          </option>
+          @endforeach
+        </select>
+      @endif
 </div>
