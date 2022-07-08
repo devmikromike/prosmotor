da8db1aeefb5f4bb50110fc06c591982c43fbb87
MikroMike <dev@mikromike.email>
2022-07-08 14:19:06 +0300

    detect enviroment fron DotEnv file

--- resources/lang/en/env.php
+++ resources/lang/en/env.php
@@ -3,6 +3,10 @@
 return [
   'alpha' => 'You are currently in alpha environment as Guest User',
   'beta' => 'You are currently in beta environment as Guest User',
-  'local' => 'You are currently in local dev environment '
-
+  'local' => 'You are currently in local dev environment ',
+  'auth' => [
+    'alpha' => 'You are currently in alpha environment as Logged User',
+    'beta' => 'You are currently in beta environment as Logged User',
+    'local' => 'You are currently in local dev environment '
+  ]
 ];
--- resources/lang/fi/env.php
+++ resources/lang/fi/env.php
@@ -3,6 +3,10 @@
 return [
   'alpha' => 'Olet tällä hetkellä alpha ympäristössä vieralevana tai sammuneena tähtenä....',
   'beta' => 'Olet tällä hetkellä beta ympäristössä vieralevana tai sammuneena tähtenä....',
-  'local' => 'Olet tällä hetkellä paikallisesti devaus ympäristössä vieralevana tai sammuneena tähtenä....'
-
+  'local' => 'Olet tällä hetkellä paikallisesti devaus ympäristössä vieralevana tai sammuneena tähtenä....',
+  'auth' => [
+    'alpha' => 'Olet tällä hetkellä alpha ympäristössä kirjautuneena.',
+    'beta' => 'Olet tällä hetkellä beta ympäristössä kirjautuneena.',
+    'local' => 'Olet tällä hetkellä paikallisesti devaus ympäristössä kirjautuneena.',
+  ]
 ];
