0d0703824a14cfc7f267f2686d8e1d322da963e1
MikroMike <dev@mikromike.email>
2022-07-18 13:07:55 +0300

    backend -WIP

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\ApiConnectionController;
 */
 
  Route::get('SearchVatID',[ApiConnectionController::class,'test']);
- Route::get('SearchVatID/{$vatId}',[ApiConnectionController::class,'index']);
+ Route::get('SearchVatID/{vatId}',[ApiConnectionController::class,'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
