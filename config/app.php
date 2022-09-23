d0a1ccfa8a47252ebdc87f58d62452ac7490a210
MikroMike <dev@mikromike.email>
2022-09-23 12:30:04 +0300

    - Updated.

--- config/app.php
+++ config/app.php
@@ -3,7 +3,7 @@
 return [
     'name' => env('APP_NAME', 'Laravel'),
     'env' => env('APP_ENV', 'production'),
-    'debug' => (bool) env('APP_DEBUG', true),
+    'debug' => (bool) env('APP_DEBUG', false),
     'url' => env('APP_URL', ''),
     'asset_url' => env('ASSET_URL', null),
     'timezone' => 'UTC',
