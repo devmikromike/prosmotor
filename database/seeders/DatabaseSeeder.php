0c98289df8706e60a59565017bc70c37432a984e
MikroMike <dev@mikromike.email>
2022-09-23 12:43:35 +0300

    - Updated.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,10 +8,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $this->call(LisenseTypeSeeder::class);
-      $this->call(RoleSeeder::class);
-      //   \App\Models\User::factory(10)->create();
+      // $this->call(RoleSeeder::class);
+         \App\Models\User::factory(10)->create();
           \App\Models\Company::factory(20)->create();
-            $this->call(UserSeeder::class);
+    ///        $this->call(UserSeeder::class);
               $this->call(LisenseSeeder::class);
                $this->call(CompanySeeder::class);
                 $this->call(ProfileSeeder::class);
